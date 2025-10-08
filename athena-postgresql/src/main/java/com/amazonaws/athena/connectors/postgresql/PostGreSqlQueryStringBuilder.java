4d05f7289bd103f4fc73ba4bb5bd45b9f4742a3a
Tanu [C] Angaria <tanangar@amazon.com>
2025-10-08 09:55:14 +0000

    added logger in master branch code

--- athena-postgresql/src/main/java/com/amazonaws/athena/connectors/postgresql/PostGreSqlQueryStringBuilder.java
+++ athena-postgresql/src/main/java/com/amazonaws/athena/connectors/postgresql/PostGreSqlQueryStringBuilder.java
@@ -25,6 +25,8 @@ import com.amazonaws.athena.connectors.jdbc.manager.FederationExpressionParser;
 import com.amazonaws.athena.connectors.jdbc.manager.JdbcSplitQueryBuilder;
 import com.google.common.base.Strings;
 import org.apache.arrow.vector.types.pojo.Schema;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -42,6 +44,7 @@ import java.util.stream.Collectors;
 public class PostGreSqlQueryStringBuilder
         extends JdbcSplitQueryBuilder
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PostGreSqlQueryStringBuilder.class);
     public PostGreSqlQueryStringBuilder(final String quoteCharacters, final FederationExpressionParser federationExpressionParser)
     {
         super(quoteCharacters, federationExpressionParser);
@@ -83,6 +86,8 @@ public class PostGreSqlQueryStringBuilder
     @Override
     protected String getFromClauseWithSplit(String catalog, String schema, String table, Split split)
     {
+        LOGGER.info("=== POSTGRES_SQL FROM CLAUSE GENERATION ===");
+        LOGGER.info("Building FROM clause for table: {}.{}.{}", catalog, schema, table);
         StringBuilder tableName = new StringBuilder();
         if (!Strings.isNullOrEmpty(catalog)) {
             tableName.append(quote(catalog)).append('.');
@@ -94,13 +99,18 @@ public class PostGreSqlQueryStringBuilder
 
         String partitionSchemaName = split.getProperty(PostGreSqlMetadataHandler.BLOCK_PARTITION_SCHEMA_COLUMN_NAME);
         String partitionName = split.getProperty(PostGreSqlMetadataHandler.BLOCK_PARTITION_COLUMN_NAME);
+        LOGGER.info("Partition Schema name from split: {}", partitionSchemaName);
+        LOGGER.info("Partition name from split: {}", partitionName);
 
         if (PostGreSqlMetadataHandler.ALL_PARTITIONS.equals(partitionSchemaName) || PostGreSqlMetadataHandler.ALL_PARTITIONS.equals(partitionName)) {
             // No partitions
-            return String.format(" FROM %s ", tableName);
+            String fromClause = String.format(" FROM %s ", tableName);
+            LOGGER.info("PostgresSQL FROM clause (no partitions): {}", fromClause);
+            return fromClause;
         }
-
-        return String.format(" FROM %s.%s ", quote(partitionSchemaName), quote(partitionName));
+        String fromClause = String.format(" FROM %s.%s ", quote(partitionSchemaName), quote(partitionName));
+        LOGGER.info("PostgresSQL FROM clause (with partition): {}", fromClause);
+        return fromClause;
     }
 
     @Override
